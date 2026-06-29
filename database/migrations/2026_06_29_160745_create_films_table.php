3a2383fb155df79594e2b8cd9415509794434626
Pavel <pavelpeper@gmail.com>
2026-06-29 17:14:14 +0300

    added migrations

---
+++ database/migrations/2026_06_29_160745_create_films_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('films', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('poster_image')->nullable();
+            $table->string('preview_image')->nullable();
+            $table->string('background_image')->nullable();
+            $table->string('background_color', 9)->nullable();
+            $table->string('video_link')->nullable();
+            $table->string('preview_video_link')->nullable();
+            $table->text('description')->nullable();
+            $table->decimal('rating', 4, 1)->nullable();
+            $table->unsignedInteger('scores_count')->default(0);
+            $table->string('director')->nullable();
+            $table->json('starring')->nullable();
+            $table->unsignedSmallInteger('run_time')->nullable();
+            $table->unsignedSmallInteger('released')->nullable();
+            $table->string('imdb_id', 20)->unique();
+            $table->enum('status', ['pending', 'moderate', 'ready'])->default('pending');
+            $table->boolean('is_promo')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('films');
+    }
+};
---
+++ database/migrations/2026_06_29_170401_create_comments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->text('text');
+            $table->unsignedTinyInteger('rating')->nullable();
+            $table->foreignId('film_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('comment_id')->nullable()->constrained('comments')->cascadeOnDelete();
+            $table->foreignId('user_id')->nullable()->constrained()->nullOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
