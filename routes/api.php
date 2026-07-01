60f083c702e3209959a05753f99e295510826f96
Pavel <pavelpeper@gmail.com>
2026-07-02 00:14:00 +0300

    added auth controllers

--- routes/api.php
+++ routes/api.php
@@ -11,28 +11,35 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout']);
 
 Route::get('/films', [FilmController::class, 'index']);
 Route::get('/films/{id}', [FilmController::class, 'show']);
-Route::post('/films', [FilmController::class, 'store']);
-Route::patch('/films/{id}', [FilmController::class, 'update']);
 Route::get('/films/{id}/similar', [FilmController::class, 'similar']);
 
 Route::get('/comments/{id}', [CommentController::class, 'index']);
-Route::post('/comments/{id}', [CommentController::class, 'store']);
-Route::patch('/comments/{comment}', [CommentController::class, 'update']);
-Route::delete('/comments/{comment}', [CommentController::class, 'destroy']);
-
-Route::get('/favorite', [FavoriteController::class, 'index']);
-Route::post('/films/{id}/favorite', [FavoriteController::class, 'store']);
-Route::delete('/films/{id}/favorite', [FavoriteController::class, 'destroy']);
-
 Route::get('/genres', [GenreController::class, 'index']);
-Route::patch('/genres/{genre}', [GenreController::class, 'update']);
-
 Route::get('/promo', [PromoController::class, 'show']);
-Route::post('/promo/{id}', [PromoController::class, 'store']);
 
-Route::get('/user', [UserController::class, 'show']);
-Route::patch('/user', [UserController::class, 'update']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/favorite', [FavoriteController::class, 'index']);
+    Route::post('/films/{id}/favorite', [FavoriteController::class, 'store']);
+    Route::delete('/films/{id}/favorite', [FavoriteController::class, 'destroy']);
+
+    Route::post('/comments/{id}', [CommentController::class, 'store']);
+    Route::patch('/comments/{comment}', [CommentController::class, 'update']);
+    Route::delete('/comments/{comment}', [CommentController::class, 'destroy']);
+
+    Route::get('/user', [UserController::class, 'show']);
+    Route::patch('/user', [UserController::class, 'update']);
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+Route::middleware(['auth:sanctum', 'role:isModerator'])->group(function () {
+    Route::post('/films', [FilmController::class, 'store']);
+    Route::patch('/films/{id}', [FilmController::class, 'update']);
+
+    Route::patch('/genres/{genre}', [GenreController::class, 'update']);
+
+    Route::post('/promo/{id}', [PromoController::class, 'store']);
+});
